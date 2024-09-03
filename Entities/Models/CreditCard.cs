d71bf6174e9d3832091277cd61384e9c6f443d8e
Raynemar <raynemar@gmail.com>
2024-09-03 17:13:25 +0300

    Entities

---
+++ Entities/Models/CreditCard.cs
@@ -0,0 +1,11 @@
+﻿namespace Entities.Models
+{
+    public class CreditCard
+    {
+        public string Id { get; set; } = string.Empty;
+        public string IdUser { get; set; } = string.Empty;
+        public string Account { get; set; } = string.Empty;
+        public DateTime Expiration { get; set; }
+        public int Cvc { get; set; }
+    }
+}
